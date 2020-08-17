22a6afd56325f5237e9c1e16799ac8f29685ddfb
ola-Mansour <oulamanso0ur@gmail.com>
2020-08-17 22:58:22 +0300

    Updates

---
+++ precache-manifest.7a0d68758509a20b16cdc6819383a061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bd85fc6d97505ae33f5cab81ca28b2",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "01f4310816499cdd674f",
+    "url": "/jammming/static/css/main.7b9ab371.chunk.css"
+  },
+  {
+    "revision": "e165caae22d703a4f69c",
+    "url": "/jammming/static/js/2.281d7517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.281d7517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01f4310816499cdd674f",
+    "url": "/jammming/static/js/main.89a67c51.chunk.js"
+  },
+  {
+    "revision": "a2d44f1a828275f3558f",
+    "url": "/jammming/static/js/runtime-main.85f7c6aa.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
